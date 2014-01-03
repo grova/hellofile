1a299d4cc1a1d7248bcf74e3812e514d581ce855
grova <giovanni.rovatti@gmail.com>
2014-01-03 17:01:35 +0100

    pulisco la download che si crea il path
    chiede il path al filesystem

--- www/js/index.js
+++ www/js/index.js
@@ -25,31 +25,36 @@ function fail(error)
  
 function downloadFile()
 {
-
-window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-    function onFileSystemSuccess(fileSystem) {
-        fileSystem.root.getFile(
-        "dummy.html", {create: true, exclusive: false}, 
-        function gotFileEntry(fileEntry) {
-            var sPath = fileEntry.fullPath.replace("dummy.html","");
-            var fileTransfer = new FileTransfer();
-            fileEntry.remove();
-
-            fileTransfer.download(
-                "http://www.storci.com/pdf/products/vsfTVmix.pdf",
-                sPath + "theFile.pdf",
-                function(theFile) {
-                    console.log("download complete: " + theFile.toURI());
-                    showLink(theFile.toURI());
-                },
-                function(error) {
-                    console.log("download error source " + error.source);
-                    console.log("download error target " + error.target);
-                    console.log("upload error code: " + error.code);
-                }
-            );
-        }, fail);
-    }, fail);
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+		function onFileSystemSuccess(fileSystem) 
+		{
+			fileSystem.root.getFile(
+				"dummy.html", {create: true, exclusive: false}, 
+				function gotFileEntry(fileEntry) 
+				{
+					var sPath = fileEntry.fullPath.replace("dummy.html","");
+					var fileTransfer = new FileTransfer();
+					console.log("filetransfer "+fileTransfer);
+					fileEntry.remove();
+					var uri = encodeURI("http://www.storci.com/pdf/products/vsfTVmix.pdf");
+					fileTransfer.download(
+						uri
+						sPath + "theFile.pdf",
+						function(theFile) {
+							console.log("download complete: " + theFile.toURI());
+						},
+						function(error) {
+						    console.log("download error source " + error.source);
+						    console.log("download error target " + error.target);
+						    console.log("upload error code: " + error.code);
+						}
+					);
+				},
+				fail
+			);
+		},
+		fail
+	);
 };
 
 function pgDownload()
@@ -120,5 +125,14 @@ var app = {
 	console.log("test");
 	downloadFile();
     },
+    
+    initFS: function()
+    {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+	
+	
+	
+	
+    }
     
 };
