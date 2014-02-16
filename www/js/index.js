54af7ae97a8257a98fbd190a79c693c9a580323a
grova <giovanni.rovatti@gmail.com>
2014-02-16 14:30:30 +0100

    push test
    push test

--- www/js/index.js
+++ www/js/index.js
@@ -897,11 +897,127 @@ var app =
 				$("#videotest").attr("src",filePath);
 			}
 		}
+    },
+
+    registerToPush: function()
+    {
+    	if ( device.platform == 'android' || device.platform == 'Android' )
+		{
+		    pushNotification.register(
+		        successHandler,
+		        errorHandler, {
+		            "senderID":"replace_with_sender_id",
+		            "ecb":"onNotificationGCM"
+		        });
+		}
+		else
+		{
+		    pushNotification.register(
+		        function(token)
+		        {
+		        	app.myAlert("device token = " + token);
+		        },
+		        function(error)
+		        {
+		        	app.myAlert("device token = " + error);
+
+		        },
+		        {
+		            "badge":"true",
+		            "sound":"true",
+		            "alert":"true",
+		            "ecb":"onNotificationAPN"
+		        });
+		}
+    },
+
+    unregisterToPush: function()
+    {
+    	pushNotification.unregister(successHandler, errorHandler);
     }
 }
 
 
+// iOS
+function onNotificationAPN (event) 
+{
+    if ( event.alert )
+    {
+        navigator.notification.alert(event.alert);
+    }
 
+    if ( event.sound )
+    {
+        var snd = new Media(event.sound);
+        snd.play();
+    }
 
+    if ( event.badge )
+    {
+        pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+    }
+}
+
+// Android
+function onNotificationGCM(e) 
+{
+    $("#app-status-ul").append('<li>EVENT -> RECEIVED:' + e.event + '</li>');
+
+    switch( e.event )
+    {
+    case 'registered':
+        if ( e.regid.length > 0 )
+        {
+            $("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+            // Your GCM push server needs to know the regID before it can push to this device
+            // here is where you might want to send it the regID for later use.
+            console.log("regID = " + e.regid);
+        }
+    break;
+
+    case 'message':
+        // if this flag is set, this notification happened while we were in the foreground.
+        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+        if ( e.foreground )
+        {
+            $("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+
+            // if the notification contains a soundname, play it.
+            var my_media = new Media("/android_asset/www/"+e.soundname);
+            my_media.play();
+        }
+        else
+        {  // otherwise we were launched because the user touched a notification in the notification tray.
+            if ( e.coldstart )
+            {
+                $("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+            }
+            else
+            {
+                $("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+            }
+        }
+
+        $("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+        $("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+    break;
+
+    case 'error':
+        $("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+    break;
+
+    default:
+        $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+    break;
+  }
+}
+
+function successHandler (result) {
+    alert('result = ' + result);
+}
+
+function errorHandler (error) {
+    alert('error = ' + error);
+}
 
 
