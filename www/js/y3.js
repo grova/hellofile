01d6b8f6e79d18d08d07e6a7d27c041e8d6c5ed9
Matteo <matteo.parenti@y3web.it>
2014-04-16 18:05:33 +0200

    ulteriore miglioramento

--- www/js/y3.js
+++ www/js/y3.js
@@ -420,7 +420,7 @@ var y3 = {
             $('#currentDownloadingSourceFile').html("downloading " + fns[fns.length-1]);
         }
         else{
-            $('#currentDownloadingSourceFile').html(app.m_currentDownloadingSourceFile);
+            $('#currentDownloadingSourceFile').html("donloading files...");
         }
     },
     
