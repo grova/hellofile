030a9d15f61bcff0eadf631615e5aacd4afd1f4e
Matteo <matteo.parenti@y3web.it>
2014-04-29 23:58:48 +0200

    ripulito options e setWiFiCheckboxState

--- www/js/y3.js
+++ www/js/y3.js
@@ -22,9 +22,7 @@ var y3 = {
     setWiFiCheckboxState: function() {
          //setto il checkbox in base al valore salvato nelle opzioni locali
          var v = app.getWifiOnly();
-         //var ov = $("#WiFiOnlyCkeckbox").prop('checked');
          $("#WiFiOnlyCkeckbox").prop("checked", v).checkboxradio("refresh");	
-         // alert('getWifiOnly() è: ' + v  + ',   -     il checkbox era: ' + ov + ' ed ora è: ' + $("#WiFiOnlyCkeckbox").prop('checked'));            
         },
     
     updateWiFiSettings: function()
