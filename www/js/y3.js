99b2662ebabdbb68bcf6d8a252bb0567a8a3be9f
Matteo <matteo.parenti@y3web.it>
2014-10-23 18:18:34 +0200

    Modificato path

--- www/js/y3.js
+++ www/js/y3.js
@@ -126,6 +126,7 @@ var y3 = {
                     if (ext == 'jpg'){ 
                         
 					fullpath = app.fileSystemRoot + "/" +app.localdb[i].localPath;
+                    fullpath = fullpath.substr(7); //tolgo file://
                         
                     t = t+("<li id='fileElement"+i+"'><a href='#' class='swipebox' src='"+fullpath+"' alt='"+fullpath+"' title='"+app.fileSystemRoot+"'><img src='img/"+y3.choseThumb(ext)+"' /><h2>"+fullpath+"</h2><p>"+app.localdb[i].localPath+"</p></a><a href='#' onclick=app.deleteFile("+app.localdb[i].fileid+"," + i + ")></a></li>"); }
                     
